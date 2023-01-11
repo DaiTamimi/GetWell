ae2eade85fb79c2150fa973e6562ff0c7ef3248b
dai <daitamimi2@gmail.com>
2023-01-11 21:16:32 +0300

    Initial Commit

---
+++ GetWell/GetWellApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetWellApp.swift
+//  GetWell
+//
+//  Created by Dai Fahad ALTamimi on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct GetWellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
